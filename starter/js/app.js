6adf7b4e33fb9b1673b75645d38636209be6c41c
ferradjielie <ferradji.elie@outlook.fr>
2023-03-27 19:19:22 +0200

    First commit

--- starter/js/app.js
+++ starter/js/app.js
@@ -7,11 +7,11 @@ const getElement = (selector) => {
   )
 }
 
-const links = getElement('.nav-links')
+const links = getElement('.nav-links') // on sélectionne la class qui s'appelle nav-links
 const navBtnDOM = getElement('.nav-btn')
 
-navBtnDOM.addEventListener('click', () => {
-  links.classList.toggle('show-links')
+navBtnDOM.addEventListener('click', () => { // on ajoute un addEventListener à la const navBtnDom
+  links.classList.toggle('show-links') // la class links a un hidden, et donc quand on clique dessus toute la nav va apparaitre grace à la class show-links ! 
 })
 
 const date = getElement('#date')
